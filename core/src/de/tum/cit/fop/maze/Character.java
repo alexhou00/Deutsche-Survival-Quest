3b5b97de8b943bba5a87cb610b026519e2e75ae6
berin <berin.buyukbodukk@gmail.com>
2024-12-13 11:38:37 +0100

    obstacle and its child classes added

--- core/src/de/tum/cit/fop/maze/Character.java
+++ core/src/de/tum/cit/fop/maze/Character.java
@@ -1,4 +1,16 @@
 package de.tum.cit.fop.maze;
 
 public abstract class Character {
+    private int lives;
+
+    public Character(int lives) {
+        this.lives = lives;
+    }
+
+    public int getLives() {
+        return lives;
+    }
+    public void setLives(int lives) {
+        this.lives = lives;
+    }
 }
--- core/src/de/tum/cit/fop/maze/Player.java
+++ core/src/de/tum/cit/fop/maze/Player.java
@@ -1,25 +1,16 @@
 package de.tum.cit.fop.maze;
 
 public class Player extends Character{
-    private int lives;
     private boolean hasKey;
     //private boolean hasMoved;
 
 
-    public Player(int lives) {
-        this.lives = lives;
-        this.hasKey = false;
-        //this.hasMoved = false;
+    public Player(int lives, boolean hasKey) {
+        super(lives);
+        this.hasKey = hasKey;
     }
-
 
     //getter and setter
-    public int getLives() {
-        return lives;
-    }
-    public void setLives(int lives) {
-        this.lives = lives;
-    }
     public boolean hasKey() {
         return hasKey;
     }
@@ -32,10 +23,4 @@ public class Player extends Character{
     public void setHasMoved(boolean hasMoved) {
         this.hasMoved = hasMoved;
     }*/
-
-
-
-
-
-
 }
