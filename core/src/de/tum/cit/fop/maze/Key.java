7b7c329d12c43083f1885e0380972e7ff56356e2
berin <berin.buyukbodukk@gmail.com>
2025-01-05 13:55:37 +0100

    working on key functionality

--- core/src/de/tum/cit/fop/maze/Key.java
+++ core/src/de/tum/cit/fop/maze/Key.java
@@ -4,8 +4,30 @@ import com.badlogic.gdx.math.Rectangle;
 
 public class Key extends Collectibles {
 
+    private boolean isCollected;
+
     public Key(String name, int worldX, int worldY, boolean collision, float width, float height, Rectangle hitbox) {
         super("key", worldX, worldY, collision, width, height, hitbox);
+        this.isCollected = false;
+    }
+
+    public boolean checkCollisionWithPlayer(Player player) {
+        if (this.getHitbox().overlaps(player.getHitbox())) {
+            return true;
+        }
+        return false;
+    }
+
+    public void collect() {
+        //to prevent multiple collections
+        if (!isCollected) {
+            isCollected = true;
+            System.out.println("Key collected!");
+        }
+    }
+
+    public boolean isCollected() {
+        return isCollected;
     }
 
 
