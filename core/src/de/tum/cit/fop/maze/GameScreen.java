00ed85e6f3cc041782cca3555f3ec39d5f35d78b
berin <berin.buyukbodukk@gmail.com>
2025-01-05 14:41:55 +0100

    key functionality but still no visible key

--- core/src/de/tum/cit/fop/maze/GameScreen.java
+++ core/src/de/tum/cit/fop/maze/GameScreen.java
@@ -428,5 +428,4 @@ public class GameScreen extends InputAdapter implements Screen {
             key.collect();
         }
     }
-
 }
--- core/src/de/tum/cit/fop/maze/Key.java
+++ core/src/de/tum/cit/fop/maze/Key.java
@@ -44,5 +44,4 @@ public class Key extends Collectibles {
         }
     }
 
-
 }
