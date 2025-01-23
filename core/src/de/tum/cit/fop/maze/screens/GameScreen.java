3deec630dcb37c35402ea3c8536021d3e68c9c77
alexhou00 <alexhou00@gmail.com>
2025-01-23 17:11:55 +0100

    Add Enemies to the tilesets for level 2, 3, 4

--- core/src/de/tum/cit/fop/maze/screens/GameScreen.java
+++ core/src/de/tum/cit/fop/maze/screens/GameScreen.java
@@ -137,8 +137,8 @@ public class GameScreen extends InputAdapter implements Screen {
 
         TiledMap tiledMap;
         switch (game.getGameLevel()) {
-            case 1,3,4,6 -> tiledMap = tiles.loadTiledMap("maps/level-"+game.getGameLevel()+"-map.properties", Gdx.files.internal("tilesets/level"+ game.getGameLevel()+"_tileset.png").path(), Gdx.files.internal("tilesets/level1_obstacles.png").path());
-            case 2 -> tiledMap = tiles.loadTiledMap("maps/level-2-map.properties", Gdx.files.internal("tilesets/level2_tileset.png").path(), Gdx.files.internal("tilesets/level2_obstacles.png").path());
+            case 1,2,3,4 -> tiledMap = tiles.loadTiledMap("maps/level-"+game.getGameLevel()+"-map.properties", Gdx.files.internal("tilesets/level"+ game.getGameLevel()+"_tileset.png").path(), Gdx.files.internal("tilesets/level"+ game.getGameLevel()+"_obstacles.png").path());
+            case 6 -> tiledMap = tiles.loadTiledMap("maps/level-"+game.getGameLevel()+"-map.properties", Gdx.files.internal("tilesets/level"+ game.getGameLevel()+"_tileset.png").path(), Gdx.files.internal("tilesets/level1_obstacles.png").path());
             //case 3 -> tiledMap = tiles.loadTiledMap("maps/level-3-map.properties", Gdx.files.internal("tilesets/level3_tileset.png").path(), Gdx.files.internal("tilesets/level1_obstacles.png").path());
             //case 4 -> tiledMap = tiles.loadTiledMap("maps/level-4-map.properties", Gdx.files.internal("tilesets/level4_tileset.png").path(), Gdx.files.internal("tilesets/level1_obstacles.png").path());
             default -> tiledMap = tiles.loadTiledMap("maps/level-1-map.properties", Gdx.files.internal("tilesets/level1_tileset.png").path(), Gdx.files.internal("tilesets/level1_obstacles.png").path());
